909e243e3b6d8c2397a8e25e526f918910ad360d
Anastasia <Klestovaa1981@gmail.com>
2024-02-23 18:38:20 +0700

    gitignore Average Cycle DigitsNumbers

---
+++ src/myJava/DigitsNumbers.java
@@ -0,0 +1,38 @@
+package myJava;
+
+import java.util.Scanner;
+
+public class DigitsNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите целое число: ");
+        int a = scanner.nextInt();
+        System.out.println(a);
+
+        int sumAllDigits = 0;
+        int sumOddDigits = 0;
+        int maxDigit = 0;
+
+        while (a != 0) {
+            int digit = a % 10;
+            sumAllDigits += digit;
+
+            if (digit % 2 != 0) {
+                sumOddDigits += digit;
+            }
+
+            if (digit > maxDigit) {
+                maxDigit = digit;
+            }
+
+            a /= 10;
+        }
+
+        System.out.println("Сумма всех цифр числа: " + sumAllDigits);
+        System.out.println("Сумма нечетных цифр числа: " + sumOddDigits);
+        System.out.println("Максимальная цифра числа: " + maxDigit);
+
+        scanner.close();
+    }
+}
