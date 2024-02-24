adc84c97a93664cdcb27d70b0827152389df0150
Anastasia <Klestovaa1981@gmail.com>
2024-02-24 14:13:15 +0700

    gitignore Average Cycle DigitsNumbers update Average update DigitsNumbers update Cycle

---
+++ src/myJava/Loop.java
@@ -0,0 +1,16 @@
+package myJava;
+
+public class Loop {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 100; i >= 1; i--) {
+            if (getMultipleOf4(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean getMultipleOf4(int num) {
+        return num % 4 == 0;
+    }
+}
